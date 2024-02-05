5b2b18736a9d644d3cda7957f86b7b09d7bed5f6
Mayank <Mayankjha237@gmail.com>
2024-02-05 15:38:02 +0530

    Java Programs

---
+++ Print_Statements/HW.java
@@ -0,0 +1,12 @@
+package Print_Statements;
+
+public class HW {
+    public static void main(String[] args) {
+        System.out.println("Hello Dunia !!!");
+        short Value_1 = 34;
+        short value_2 = 45;
+        int add = value_2+Value_1;
+        System.out.println(add);
+    }
+
+}
---
+++ Print_Statements/Hello_Dunia.java
@@ -0,0 +1,7 @@
+package Print_Statements;
+
+public class Hello_Dunia {
+    public static void main(String[] args) {
+        System.out.println("Hello Dunia");
+    }
+}
