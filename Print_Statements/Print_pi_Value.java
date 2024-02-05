3c2770698ed7dcab7a1425fa7992eadf05d7c8fd
Mayank <Mayankjha237@gmail.com>
2024-02-05 15:39:24 +0530

    Java Programs

--- Print_Statements/Print_pi_Value.java
+++ Print_Statements/Print_pi_Value.java
@@ -5,7 +5,7 @@ public class Print_pi_Value {
         final float pi = 3.14f; // Final word just works like Constant
         System.out.println("The value of Pi is : "+ pi);
         System.out.println("The thing is you can use Final with every data type but you can't change the value Once Assigned");
-
+ // okay
 //        int pi = 6;
 //        System.out.println(pi);
 //        // as you can se if we declare the constant then we will not be able to change the value
