91f5cdc52546f0545aed4ddd02fb9ba4d870bf5f
Mayank <Mayankjha237@gmail.com>
2024-02-05 16:05:24 +0530

    add

---
+++ Print_Statements/Add.java
@@ -0,0 +1,11 @@
+package Print_Statements;
+
+public class Add {
+    public static void main(String[] args) {
+        int a = 45;
+        int b = 67;
+        int add = a+b;
+        System.out.println("The value of "+ a + "and " + b + "is : " +add );
+//        This is called the Concatination in System.out.println();
+    }
+}
