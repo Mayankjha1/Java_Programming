164735c0d63fd32b43bdaea2a263af394a4be5b2
Mayank <Mayankjha237@gmail.com>
2024-02-05 15:57:57 +0530

    Java Programs

---
+++ Print_Statements/Testing.java
@@ -0,0 +1,7 @@
+package Print_Statements;
+
+public class Testing {
+    public static void main(String[] args) {
+        System.out.print("This is only for test and this is for testing purpose");
+    }
+}
