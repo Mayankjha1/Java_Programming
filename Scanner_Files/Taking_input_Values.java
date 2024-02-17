b9f9fafda0a2acc80144d60cd17b4314cfce66ca
Mayank <Mayankjha237@gmail.com>
2024-02-17 11:56:01 +0530

    qu

--- Scanner_Files/Taking_input_Values.java
+++ Scanner_Files/Taking_input_Values.java
@@ -1,2 +1,20 @@
-package Scanner_Files;public class Taking_input_Values {
+package Scanner_Files;
+
+import java.util.Scanner;
+
+public class Taking_input_Values {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name !!!");
+        String Name = sc.next();
+        System.out.println("Enter your age !!!");
+        int age = sc.nextInt();
+        System.out.println("Enter your Salary !!!");
+        int salary = sc.nextInt();
+
+        System.out.println("Your Name is : " + Name +"\n" + "Your age is : "+ age +"\n"+ "Your Salary is : "+ salary);
+
+
+
+    }
 }
