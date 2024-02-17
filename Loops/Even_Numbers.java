05b7772f39444610921cda5b61afd1fb5eab5fbd
Mayank <Mayankjha237@gmail.com>
2024-02-17 13:18:07 +0530

    folders

---
+++ Loops/Even_Numbers.java
@@ -0,0 +1,11 @@
+package Loops;
+
+public class Even_Numbers {
+    public static void main(String[] args) {
+        for (int i = 1; i <=50 ; i++) {
+            if(!(i%2 == 1)){
+                System.out.println(i + " Even");
+            }
+        }
+    }
+}
---
+++ Loops/Print_odd_numbers.java
@@ -0,0 +1,11 @@
+package Loops;
+
+public class Print_odd_numbers {
+    public static void main(String[] args) {
+        for (int i = 1; i <=50; i++) {
+            if (i%2 == 1) {
+                System.out.println(i + " odd");
+            }
+        }
+    }
+}
